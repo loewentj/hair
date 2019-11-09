eb5442dd7aa8897a2bb0dd83b9baaff56896b11e
loewentj <38169321+loewentj@users.noreply.github.com>
2019-11-09 11:22:30 +1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+var navbar = document.querySelector("#main-nav");
+var sticky = navbar.offsetTop;
+window.onscroll =  () =>{
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+}
+
+
+
+// window.onscroll = function() {myFunction()};
+
+
+
+
+
+
+// var navbar = document.getElementById("nav-list");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
